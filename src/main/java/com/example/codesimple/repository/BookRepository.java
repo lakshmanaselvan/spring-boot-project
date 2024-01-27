b4e519d42c2fa2797f21af7df609d260b8c1e618
lakshmanaselvan <lakshmanaselvan252@gmail.com>
2024-01-27 14:06:43 +0530

    Second Commit

--- src/main/java/com/example/codesimple/repository/BookRepository.java
+++ src/main/java/com/example/codesimple/repository/BookRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Set;
 
 @Repository
-public interface BookRepository extends JpaRepository<Book, Integer> {
+public interface BookRepository extends JpaRepository<Book, Long> {
 
 
 
